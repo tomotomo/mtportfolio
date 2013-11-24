b25025f10f3ea119b010638553361df72fa499be
mt8 <mt8.biz@gmail.com>
2013-11-24 12:14:35 +0900

    検索フォームを正しく動作させる

--- header.php
+++ header.php
@@ -55,13 +55,7 @@ wp_enqueue_script(
         
         <div class="site-search">
 			<aside id="search-2" class="widget widget_search">
-			<form role="search" method="get" class="search-form" action="./index_files/index.html">
-			<label>
-				<span class="screen-reader-text">Search for:</span>
-				<input type="search" class="search-field" placeholder="Search …" value="" name="s">
-			</label>
-			<input type="submit" class="search-submit" value="Search">
-			</form>
+				<?php get_search_form(); ?>
 			</aside>
 		</div>
 
