78f06f6d0c877ea0ea4c475d41278db017b719fa
kozaru <nobuko@corogari.com>
2013-11-27 15:56:07 +0900

    page-topをScrollしたときに表示させる:kozaru

--- js/frontend.js
+++ js/frontend.js
@@ -4,10 +4,21 @@
 ;
 (function($) {
 	$(function() {
-
 		// Scroll to page top
 		$('.page-top a').click(function() {
 			$('html,body').animate({scrollTop: 0}, 'fast');
+			return false;
+		});
+	});
+})(jQuery);
+
+(function($){
+	$(function() {
+		// When the page scroll, the button display at page-bottom.
+		var a=$(".page-top");
+		a.hide();
+		$(window).scroll(function(){
+			150<$(this).scrollTop()?a.fadeIn():a.fadeOut();
 			return false;
 		});
 	});
