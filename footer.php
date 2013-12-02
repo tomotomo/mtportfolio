15b619fd108e0e955aaa33f0b27d18c4168706ef
mt8 <mt8.biz@gmail.com>
2013-12-02 21:58:58 +0900

    Theme Check Info : ハードリンクを削除

--- footer.php
+++ footer.php
@@ -32,17 +32,12 @@
 	</div><!-- .footer-widget-area -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-    	<div class="site-info">Copyright(c)mtportfolio</div><!-- .site-info -->
+    	<div class="site-info">
+			<?php do_action( 'mtportfolio_credits' ); ?>
+			Copyright(c)mtportfolio
+		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 
-	<!-- <footer id="colophon" class="site-footer" role="contentinfo"> -->
-		<!-- <div class="site-info">
-			<?php do_action( 'mtportfolio_credits' ); ?>
-			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'mtportfolio' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'mtportfolio' ), 'mtportfolio', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div> --><!-- .site-info -->
-	<!-- </footer> --><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
